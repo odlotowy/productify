df2929f3206658196bc6cb7418a3c582ddd780a4
Antoni <antiolos17@gmail.com>
2026-01-03 23:06:03 +0100

    feat: integrate Clerk for authentication and add routing
    - Added Clerk for user authentication with SignIn and SignUp buttons.
    - Implemented routing with React Router for Home, Product, Profile, Create, and Edit pages.
    - Created a Navbar component with links and theme selector.
    - Introduced Tailwind CSS for styling and removed old CSS files.
    - Added new pages: HomePage, ProductPage, ProfilePage, CreatePage, and EditProductsPage.
    - Removed unused assets and CSS files.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { ClerkProvider } from '@clerk/clerk-react';
+import { BrowserRouter } from 'react-router';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+	throw new Error('Missing Publishable key');
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</ClerkProvider>
+	</StrictMode>
+);
